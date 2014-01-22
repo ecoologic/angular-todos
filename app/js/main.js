091c300b6ec18811c62e6b434250e9455aed01e4
erik <erikecoologic@gmail.com>
2014-01-22 22:35:07 +1000

    more restful todos + checkbox display

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,11 @@
 
 var mainApp = angular.module('mainApp', ['firebase']);
 
-function MainCtrl($scope, $firebase) {
+function TodosCtrl($scope, $firebase) {
   var todosRef = new Firebase('https://ecoologic-todos.firebaseio.com/todos');
   $scope.todos = $firebase(todosRef);
 
-  $scope.addTodo = function () {
+  $scope.create = function () {
     $scope.todos.$add({
       title: $scope.newTodoTitle,
       points: $scope.newTodoPoints,
