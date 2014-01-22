ac99896dd02c4a753fff4b21cb55408506740d7f
erik <erikecoologic@gmail.com>
2014-01-22 20:52:53 +1000

    todos with attributes

--- app/js/main.js
+++ app/js/main.js
@@ -3,11 +3,15 @@
 var mainApp = angular.module('mainApp', ['firebase']);
 
 function MainCtrl($scope, $firebase) {
-  var todosRef = new Firebase('https://ecoologic-todos.firebaseio.com/simple-todos');
+  var todosRef = new Firebase('https://ecoologic-todos.firebaseio.com/todos');
   $scope.todos = $firebase(todosRef);
 
   $scope.addTodo = function () {
-    $scope.todos.$add($scope.newTodo);
-    $scope.newTodo = '';
+    $scope.todos.$add({
+      title: $scope.newTodoTitle,
+      points: $scope.newTodoPoints,
+      completed: false
+    });
+    $scope.newTodoTitle = '';
   };
 };
