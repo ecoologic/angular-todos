fc499f13d1341f2cf1d4c0dfdaad2ec66a0f0e74
erik <erikecoologic@gmail.com>
2014-03-13 22:39:53 +1000

    wip xeditable

--- app/js/main.js
+++ app/js/main.js
@@ -37,8 +37,12 @@ controllers.TodosCtrl = function($scope, $firebase) {
 
 // app
 var dependencies = [
-  'firebase'        // https://www.firebase.com/quickstart/angularjs.html
+  'firebase',        // https://www.firebase.com/quickstart/angularjs.html
+  'xeditable'        // http://vitalets.github.io/angular-xeditable/
 ];
 var app = angular.module('app', dependencies)
                  .controller(controllers);
 
+app.run(function (editableOptions) {
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
