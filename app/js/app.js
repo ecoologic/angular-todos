08dbc063c8747cc950ec675f099d973fef8b8cdb
erik <erikecoologic@gmail.com>
2014-04-02 21:25:05 +1000

    log and activeTodo (working!)

--- app/js/app.js
+++ app/js/app.js
@@ -25,11 +25,16 @@ controllers.TodosCtrl = function($scope, Store) {
     $scope.total     = $scope.todos.$getIndex().length;
   });
 
+  $scope.setActiveTodo = function (id) {
+    $scope.activeTodo = $scope.todos.$child(id);
+  };
+
   $scope.delete = function (id) { $scope.todos.$remove(id); };
 
   $scope.update = function (id) { $scope.todos.$save(id); };
 
   $scope.create = function () {
+    console.log('create', $scope.activeTodo);
     $scope.activeTodo.$child(
       encodeURIComponent($scope.newTodo.title)
     ).$set({
@@ -50,15 +55,23 @@ services.Store = function ($firebase) {
   _.each(resourceNames, function (resourceName) {
     var resourceUrl = firebaseUrl + resourceName,
         resource = $firebase(new Firebase(resourceUrl));
+
     resource.$url = resourceUrl;
+
     resource.$properties = function () {
       return _.map(resource.$getIndex(), function (id) {
         return resource.$child(id);
       });
     };
+
+    resource.$halfChild = function (id) {
+      var result = resource[id];
+      result.$id = id;
+      return result;
+    };
+
     result[resourceName] = resource;
   });
-
   return result;
 };
 var filters = {}; /////////////////////////////////////////////////////////////
@@ -69,13 +82,14 @@ filters.sortBy = function () {
       var sortedIds = _.sortBy(ids, function (id) {
         return list[id][sortAttribute];
       });
-      result = _.map(sortedIds, function (id) { return list[id]; });
+      result = _.map(sortedIds, function (id) { return list.$halfChild(id); });
     };
     console.log("filter", result || list);
     return result || list;
   };
 };
-var run = function (editableOptions) { ////////////////////////////////////////
+var run = function ($rootScope, $log, editableOptions) { //////////////////////
+  $rootScope.$log = $log;
   editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 };
 var dependencies = [ //////////////////////////////////////////////////////////
